c5add8279c3ec1487b1ae8474cff05216612bf3a
Abner Tudtud <114082473+enjinabner@users.noreply.github.com>
2023-06-27 10:42:32 +0800

    Batch transaction by collection (#5)

---
+++ src/Events/BeamBatchTransactionCreated.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Enjin\Platform\Beam\Events;
+
+use Enjin\Platform\Beam\Channels\PlatformBeamChannel;
+use Enjin\Platform\Channels\PlatformAppChannel;
+use Enjin\Platform\Events\PlatformBroadcastEvent;
+
+class BeamBatchTransactionCreated extends PlatformBroadcastEvent
+{
+    /**
+     * Creates a new event instance.
+     */
+    public function __construct(string $collectionId, int $transactionId)
+    {
+        parent::__construct();
+
+        $this->broadcastData = [
+            'collectionId' => $collectionId,
+            'transactionId' => $transactionId,
+        ];
+
+        $this->broadcastChannels = [
+            new PlatformAppChannel(),
+            new PlatformBeamChannel(),
+        ];
+    }
+}
