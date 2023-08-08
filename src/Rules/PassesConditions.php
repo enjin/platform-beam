263a8a9e298875ee08eb0346216a429601ab0700
Simon Evans <simon@enjin.io>
2023-08-08 01:39:03 +0100

    Implement PassesConditions rule for custom conditional logic.

---
+++ src/Rules/PassesConditions.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Enjin\Platform\Beam\Rules;
+
+use Closure;
+use Illuminate\Contracts\Validation\DataAwareRule;
+use Illuminate\Contracts\Validation\Rule;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class PassesConditions implements DataAwareRule, ValidationRule
+{
+    public static array $functions = [];
+
+    protected array $data = [];
+
+    /**
+     * Create new rule instance.
+     */
+    public function __construct(
+        protected bool $singleUse
+    ) {
+    }
+
+    /**
+     * Get the validation error message.
+     */
+    public function message()
+    {
+        return __('enjin-platform-beam::validation.passes_conditions');
+    }
+
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        $conditions = collect(static::$functions);
+
+        if (!$conditions->every(fn ($function) => $function($value, $this->singleUse, $this->data))) {
+            $fail(__('enjin-platform-beam::validation.passes_conditions'));
+        }
+    }
+
+    public function setData(array $data)
+    {
+        $this->data = $data;
+
+        return $this;
+    }
+}
--- tests/Feature/GraphQL/Mutations/ClaimBeamTest.php
+++ tests/Feature/GraphQL/Mutations/ClaimBeamTest.php
@@ -10,6 +10,7 @@ use Enjin\Platform\Beam\Enums\BeamType;
 use Enjin\Platform\Beam\Events\BeamClaimPending;
 use Enjin\Platform\Beam\Jobs\ClaimBeam;
 use Enjin\Platform\Beam\Models\BeamClaim;
+use Enjin\Platform\Beam\Rules\PassesConditions;
 use Enjin\Platform\Beam\Tests\Feature\GraphQL\TestCaseGraphQL;
 use Enjin\Platform\Beam\Tests\Feature\Traits\CreateBeamData;
 use Enjin\Platform\Beam\Tests\Feature\Traits\SeedBeamData;
@@ -101,6 +102,56 @@ class ClaimBeamTest extends TestCaseGraphQL
     public function test_it_can_claim_beam_with_ed25519(): void
     {
         $this->genericClaimTest(CryptoSignatureType::ED25519);
+    }
+
+    /**
+     * Test claiming beam with passing conditions.
+     */
+    public function test_it_can_claim_beam_with_conditions_that_pass(): void
+    {
+        PassesConditions::$functions[] = function ($code, $singleUse, $data) {
+            return CryptoSignatureType::ED25519->name == $data['cryptoSignatureType'];
+        };
+
+        $this->genericClaimTest(CryptoSignatureType::ED25519);
+
+        PassesConditions::$functions = [];
+    }
+
+    /**
+     * Test claiming beam with failing conditions.
+     */
+    public function test_it_cannot_claim_beam_with_conditions_that_fail(): void
+    {
+        PassesConditions::$functions[] = function ($code, $singleUse, $data) {
+            return CryptoSignatureType::SR25519->name == $data['cryptoSignatureType'];
+        };
+
+        [$keypair, $publicKey, $privateKey] = $this->getKeyPair(CryptoSignatureType::ED25519);
+
+        $response = $this->graphql('GetBeam', [
+            'code' => $this->beam->code,
+            'account' => $publicKey,
+        ]);
+        $this->assertNotEmpty($response['message']);
+
+        $message = $response['message']['message'];
+        $signature = $this->signMessage(CryptoSignatureType::ED25519, $keypair, $message, $privateKey);
+
+        Queue::fake();
+
+        $response = $this->graphql($this->method, [
+            'code' => $this->beam->code,
+            'account' => $publicKey,
+            'signature' => $signature,
+            'cryptoSignatureType' => CryptoSignatureType::ED25519->name,
+        ], true);
+
+        $this->assertNotEmpty($response);
+
+        $this->assertArraySubset(['code' => ['Not all the conditions to claim have been met.']], $response['error']);
+
+        PassesConditions::$functions = [];
     }
 
     /**
