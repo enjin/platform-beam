b44f4194edfc843b8d373d7849a73f26203240ca
Abner Tudtud <abner@enjin.io>
2024-01-24 09:50:06 +0800

    Scope queries and mutations

---
+++ src/Rules/BeamExists.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Enjin\Platform\Beam\Rules;
+
+use Closure;
+use Enjin\Platform\Beam\Models\Beam;
+use Illuminate\Contracts\Validation\ValidationRule;
+
+class BeamExists implements ValidationRule
+{
+    public function __construct(protected string $column = 'code')
+    {
+    }
+
+    /**
+     * Run the validation rule.
+     *
+     * @param  \Closure(string): \Illuminate\Translation\PotentiallyTranslatedString  $fail
+     */
+    public function validate(string $attribute, mixed $value, Closure $fail): void
+    {
+        if (!Beam::where([$this->column => $value])->exists()) {
+            $fail('validation.exists')->translate();
+        }
+    }
+}
