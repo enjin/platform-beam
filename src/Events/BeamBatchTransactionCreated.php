db550da14095a4d815c8732ca0a9afd4d6c5f368
Leonardo Custodio <leonardo@custodio.me>
2024-06-07 10:40:22 -0300

    Update events

--- src/Events/BeamBatchTransactionCreated.php
+++ src/Events/BeamBatchTransactionCreated.php
@@ -5,20 +5,21 @@ namespace Enjin\Platform\Beam\Events;
 use Enjin\Platform\Beam\Channels\PlatformBeamChannel;
 use Enjin\Platform\Channels\PlatformAppChannel;
 use Enjin\Platform\Events\PlatformBroadcastEvent;
+use Illuminate\Database\Eloquent\Model;
 
 class BeamBatchTransactionCreated extends PlatformBroadcastEvent
 {
     /**
      * Creates a new event instance.
      */
-    public function __construct(string $beamId, string $collectionId, int $transactionId)
+    public function __construct(mixed $event, ?Model $transaction, ?array $extra)
     {
         parent::__construct();
 
         $this->broadcastData = [
-            'beamId' => $beamId,
-            'collectionId' => $collectionId,
-            'transactionId' => $transactionId,
+            'beamId' => $event['beam_id'],
+            'collectionId' => $event['collection_id'],
+            'transactionId' => $transaction?->id,
         ];
 
         $this->broadcastChannels = [
