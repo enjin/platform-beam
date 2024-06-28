0b4ffedc3cef53b86c2e6fa5e1d92294ec8f56e1
Simon Evans <simon@enjin.io>
2024-06-28 22:10:14 +0100

    [PLA-1693] Improved local testing.

--- src/Package.php
+++ src/Package.php
@@ -3,21 +3,10 @@
 namespace Enjin\Platform\Beam;
 
 use Enjin\Platform\Beam\Enums\BeamRoute;
+use Enjin\Platform\Package as CorePackage;
 
-class Package
+class Package extends CorePackage
 {
-    /**
-     * Get the composer autoloader for auto-bootstrapping services.
-     */
-    public static function getAutoloader(): string
-    {
-        // TODO: It doesn't work on a real laravel env if using the below
-        // $loader = require realpath(__DIR__ . 'vendor/autoload.php');
-        // TODO: If using the below it doesn't work on testbench, neither composer will install
-        // $loader = require app()->basePath('../../../autoload.php')
-        return app()->runningUnitTests() ? require app()->basePath('../../../autoload.php') : require app()->basePath('vendor/autoload.php');
-    }
-
     /**
      * Get any routes that have been set up for this package.
      */
