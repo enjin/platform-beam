5f7dfd899d6ea9feb68f8eadfbe163776ee2f053
Abner Tudtud <abner@enjin.io>
2024-09-02 22:32:28 +0800

    Optimize remove claim token listener

--- src/Listeners/RemoveClaimToken.php
+++ src/Listeners/RemoveClaimToken.php
@@ -2,9 +2,12 @@
 
 namespace Enjin\Platform\Beam\Listeners;
 
+use Enjin\Platform\Beam\Models\Beam;
 use Enjin\Platform\Beam\Models\BeamClaim;
 use Enjin\Platform\Beam\Services\BeamService;
 use Enjin\Platform\Events\PlatformBroadcastEvent;
+use Enjin\Platform\Models\Collection;
+use Enjin\Platform\Models\Token;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Cache;
 
@@ -15,21 +18,30 @@ class RemoveClaimToken implements ShouldQueue
      */
     public function handle(PlatformBroadcastEvent $event): void
     {
-        BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
-            ->whereHas(
-                'collection',
-                fn ($query) => $query->where('collection_chain_id', $event->broadcastData['collectionId'])
-            )
-            ->claimable()
-            ->with('beam')
-            ->get()
-            ->each(function ($claim) {
-                if ($claim->token?->nonFungible) {
-                    $claim->delete();
-                    if ($code = $claim?->beam?->code) {
-                        Cache::decrement(BeamService::key($code));
-                    }
-                }
-            });
+        if (!$collection = Collection::where('collection_chain_id', $event->broadcastData['collectionId'])->first()) {
+            return;
+        }
+
+        if (Token::where('collection_id', $collection->id)
+            ->where('token_chain_id', $event->broadcastData['tokenId'])
+            ->first()
+            ?->nonFungible
+        ) {
+            $beamsToDecrement = BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
+                ->where('collection_id', $collection->id)
+                ->claimable()
+                ->pluck('beam_id');
+
+            BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
+                ->where('collection_id', $collection->id)
+                ->claimable()
+                ->delete();
+
+            Beam::whereIn('id', $beamsToDecrement)
+                ->get(['id,code'])
+                ->each(fn ($beam) => Cache::decrement(BeamService::key($beam->code)));
+        }
+
+
     }
 }
