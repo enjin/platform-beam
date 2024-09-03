b60265f6e9ab2e1131a153c773b9952e6fb7279e
Abner Tudtud <114082473+enjinabner@users.noreply.github.com>
2024-09-03 16:22:48 +0800

    [PLA-1970] Optimize remove claim token listener (#95)

--- src/Listeners/RemoveClaimToken.php
+++ src/Listeners/RemoveClaimToken.php
@@ -2,9 +2,12 @@
 
 namespace Enjin\Platform\Beam\Listeners;
 
+use Enjin\Platform\Beam\Models\Beam;
 use Enjin\Platform\Beam\Models\BeamClaim;
 use Enjin\Platform\Beam\Services\BeamService;
 use Enjin\Platform\Events\PlatformBroadcastEvent;
+use Enjin\Platform\Models\Collection;
+use Enjin\Platform\Models\Token;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Support\Facades\Cache;
 
@@ -15,21 +18,41 @@ class RemoveClaimToken implements ShouldQueue
      */
     public function handle(PlatformBroadcastEvent $event): void
     {
-        BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
-            ->whereHas(
-                'collection',
-                fn ($query) => $query->where('collection_chain_id', $event->broadcastData['collectionId'])
-            )
-            ->claimable()
-            ->with('beam')
-            ->get()
-            ->each(function ($claim) {
-                if ($claim->token?->nonFungible) {
-                    $claim->delete();
-                    if ($code = $claim?->beam?->code) {
-                        Cache::decrement(BeamService::key($code));
-                    }
+        $collectionId = Cache::remember(
+            'collection:' . $event->broadcastData['collectionId'],
+            30,
+            fn () => Collection::where('collection_chain_id', $event->broadcastData['collectionId'])->value('id')
+        );
+
+        if (!$collectionId) {
+            return;
+        }
+
+        $key = 'RemoveClaimToken:' . $collectionId . ':' . $event->broadcastData['tokenId'];
+        Cache::lock($key, 5)->get(function () use ($event, $collectionId) {
+            if (Token::where('collection_id', $collectionId)
+                ->where('token_chain_id', $event->broadcastData['tokenId'])
+                ->first()
+                ?->nonFungible
+            ) {
+                $beamsToDecrement = BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
+                    ->where('collection_id', $collectionId)
+                    ->selectRaw('beam_id, count(*) total')
+                    ->claimable()
+                    ->groupBy('beam_id')
+                    ->pluck('total', 'beam_id');
+
+                if (count($beamsToDecrement)) {
+                    BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
+                        ->where('collection_id', $collectionId)
+                        ->claimable()
+                        ->delete();
+
+                    Beam::whereIn('id', $beamsToDecrement->keys())
+                        ->get(['id', 'code'])
+                        ->each(fn ($beam) => Cache::decrement(BeamService::key($beam->code), $beamsToDecrement[$beam->id]));
                 }
-            });
+            }
+        });
     }
 }
