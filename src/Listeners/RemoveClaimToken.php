648b3b6f538d61fc3a338a3424c5cac640364b90
Abner Tudtud <abner@enjin.io>
2024-09-03 15:37:37 +0800

    PR comments

--- src/Listeners/RemoveClaimToken.php
+++ src/Listeners/RemoveClaimToken.php
@@ -18,7 +18,13 @@ class RemoveClaimToken implements ShouldQueue
      */
     public function handle(PlatformBroadcastEvent $event): void
     {
-        if (!$collection = Collection::where('collection_chain_id', $event->broadcastData['collectionId'])->first()) {
+        $collection = Cache::remember(
+            'collection:' . $event->broadcastData['collectionId'],
+            30,
+            fn () => Collection::where('collection_chain_id', $event->broadcastData['collectionId'])->first(['id'])
+        );
+
+        if (!$collection) {
             return;
         }
 
@@ -29,17 +35,19 @@ class RemoveClaimToken implements ShouldQueue
         ) {
             $beamsToDecrement = BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
                 ->where('collection_id', $collection->id)
+                ->selectRaw('beam_id, count(*) total')
                 ->claimable()
-                ->pluck('beam_id');
+                ->groupBy('beam_id')
+                ->pluck('total', 'beam_id');
 
             BeamClaim::where('token_chain_id', $event->broadcastData['tokenId'])
                 ->where('collection_id', $collection->id)
                 ->claimable()
                 ->delete();
 
-            Beam::whereIn('id', $beamsToDecrement)
+            Beam::whereIn('id', $beamsToDecrement->keys())
                 ->get(['id', 'code'])
-                ->each(fn ($beam) => Cache::decrement(BeamService::key($beam->code)));
+                ->each(fn ($beam) => Cache::decrement(BeamService::key($beam->code), $beamsToDecrement[$beam->id]));
         }
 
 
