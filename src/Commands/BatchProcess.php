96d4515a083f8943c07a3626cec29e61a35a7f1d
Abner Tudtud <abner@enjin.io>
2024-09-24 12:50:15 +0800

    Add transfer callbacks

--- src/Commands/BatchProcess.php
+++ src/Commands/BatchProcess.php
@@ -31,6 +31,11 @@ class BatchProcess extends Command
     public static $signingAccountResolver;
 
     /**
+     * Adhoc functions before transfer.
+     */
+    public static $beforeTransferCallbacks = [];
+
+    /**
      * The name and signature of the console command.
      *
      * @var string
@@ -154,6 +159,7 @@ class BatchProcess extends Command
                     $params[$collectionId]['beamId'] = $claim->beam_id;
 
                     if ($type == BeamType::TRANSFER_TOKEN) {
+                        $this->runBeforeTransferCallbacks($claim);
                         $daemon = Account::daemonPublicKey();
                         $params[$collectionId]['recipients'][] = [
                             'accountId' => $claim->wallet_public_key,
@@ -256,5 +262,12 @@ class BatchProcess extends Command
         }
 
         return null;
+    }
+
+    protected function runBeforeTransferCallbacks(array $claim)
+    {
+        foreach (static::$beforeTransferCallbacks as $callback) {
+            call_user_func($callback, [$claim]);
+        }
     }
 }
