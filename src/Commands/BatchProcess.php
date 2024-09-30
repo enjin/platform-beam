1cfc7205d536b0f089c9ce8d2b20b558fabcd944
Abner Tudtud <abner@enjin.io>
2024-09-30 20:56:10 +0800

    lint

--- src/Commands/BatchProcess.php
+++ src/Commands/BatchProcess.php
@@ -166,7 +166,7 @@ class BatchProcess extends Command
                                 'tokenId' => ['integer' => $claim->token_chain_id],
                                 'amount' => $claim->quantity,
                                 'keepAlive' => false,
-                                'source' => match(true) {
+                                'source' => match (true) {
                                     resolve(CollectionService::class)->approvalExistsInCollection($collectionId, $daemon, false) => $daemon,
                                     $daemon !== $claim->collection->owner->public_key => $claim->collection->owner->public_key,
                                     default => null
