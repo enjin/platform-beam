e1dbf92625ff16b85765bebd440f11c7dfa1ab0a
Abner Tudtud <abner@enjin.io>
2024-10-09 14:37:47 +0800

    Revert "Check wallet daemon approval (#99)"
    This reverts commit 0430a28efbe8a062692e7fa11606f827dc6d68e8.

--- src/Commands/BatchProcess.php
+++ src/Commands/BatchProcess.php
@@ -13,7 +13,6 @@ use Enjin\Platform\Enums\Substrate\TokenMintCapType;
 use Enjin\Platform\GraphQL\Schemas\Primary\Substrate\Mutations\BatchMintMutation;
 use Enjin\Platform\Models\Token;
 use Enjin\Platform\Services\Blockchain\Implementations\Substrate;
-use Enjin\Platform\Services\Database\CollectionService;
 use Enjin\Platform\Services\Database\TransactionService;
 use Enjin\Platform\Services\Serialization\Interfaces\SerializationServiceInterface;
 use Enjin\Platform\Support\Account;
@@ -153,19 +152,16 @@ class BatchProcess extends Command
 
                     $params[$collectionId]['beamId'] = $claim->beam_id;
 
-                    if ($type == BeamType::TRANSFER_TOKEN) {
-                        $daemon = Account::daemonPublicKey();
+                    if (BeamType::TRANSFER_TOKEN == $type) {
                         $params[$collectionId]['recipients'][] = [
                             'accountId' => $claim->wallet_public_key,
                             'params' => $this->substrate->getTransferParams([
                                 'tokenId' => ['integer' => $claim->token_chain_id],
                                 'amount' => $claim->quantity,
                                 'keepAlive' => false,
-                                'source' => match (true) {
-                                    resolve(CollectionService::class)->approvalExistsInCollection($collectionId, $daemon) => $daemon,
-                                    $daemon !== $claim->collection->owner->public_key => $claim->collection->owner->public_key,
-                                    default => null
-                                },
+                                'source' => Account::daemonPublicKey() !== $claim->collection->owner->public_key
+                                    ? $claim->collection->owner->public_key
+                                    : null,
                             ])->toEncodable(),
                         ];
                     } else {
