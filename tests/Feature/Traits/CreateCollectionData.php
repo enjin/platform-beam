a1fafba8452fe16a85f5b47ae9187dedf8fc4b13
Abner Tudtud <abner@enjin.io>
2024-10-11 10:34:50 +0800

    Revert

--- tests/Feature/Traits/CreateCollectionData.php
+++ tests/Feature/Traits/CreateCollectionData.php
@@ -2,6 +2,7 @@
 
 namespace Enjin\Platform\Beam\Tests\Feature\Traits;
 
+use Enjin\Platform\Enums\Substrate\TokenMintCapType;
 use Enjin\Platform\Models\Collection;
 use Enjin\Platform\Models\Token;
 use Enjin\Platform\Models\TokenAccount;
@@ -59,7 +60,7 @@ trait CreateCollectionData
             'collection_id' => $this->collection->id,
             'token_chain_id' => (string) fake()->unique()->numberBetween(2000),
             'supply' => (string) $supply = fake()->numberBetween(1),
-            'cap' => null,
+            'cap' => TokenMintCapType::COLLAPSING_SUPPLY->name,
             'cap_supply' => null,
             'is_frozen' => false,
             'unit_price' => (string) $unitPrice = fake()->numberBetween(1 / $supply * 10 ** 17),
