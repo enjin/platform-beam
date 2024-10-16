2597436c2b9c202a425cf5da100cf150c37a792e
Abner Tudtud <abner@enjin.io>
2024-10-16 15:09:57 +0800

    Allow claiming on frozen collection

--- src/Listeners/PauseBeam.php
+++ src/Listeners/PauseBeam.php
@@ -2,7 +2,9 @@
 
 namespace Enjin\Platform\Beam\Listeners;
 
+use Enjin\Platform\Beam\Enums\BeamType;
 use Enjin\Platform\Beam\Models\Beam;
+use Enjin\Platform\Beam\Models\BeamClaim;
 use Enjin\Platform\Beam\Services\BeamService;
 use Enjin\Platform\Events\PlatformBroadcastEvent;
 use Illuminate\Contracts\Queue\ShouldQueue;
@@ -15,15 +17,26 @@ class PauseBeam implements ShouldQueue
      */
     public function handle(PlatformBroadcastEvent $event): void
     {
-        Beam::where('collection_chain_id', $event->broadcastData['collectionId'])
-            ->get()
-            ->each(function ($beam) {
-                $beam->update(['flags_mask' => BeamService::getFlagsValue(
-                    collect(array_merge($beam->flags ?? [], ['PAUSED']))
-                        ->unique()
-                        ->map(fn ($flag) => ['flag' => $flag, 'enabled' => true])->all()
-                )]);
-                Log::info("Pausing beam {$beam->code} cause the collection {$beam->collection_chain_id} was paused.");
-            });
+        foreach (Beam::where('collection_chain_id', $event->broadcastData['collectionId'])->get() as $beam) {
+            $hasTransfers = BeamClaim::claimable()
+                ->where('type', BeamType::TRANSFER_TOKEN->name)
+                ->where('beam_id', $beam->id)
+                ->exists();
+            $hasMints = BeamClaim::claimable()
+                ->where('type', BeamType::MINT_ON_DEMAND->name)
+                ->where('beam_id', $beam->id)
+                ->exists();
+            if (($hasMints && !$hasTransfers) || ($hasMints && $hasTransfers)) {
+                continue;
+            }
+
+            $beam->update(['flags_mask' => BeamService::getFlagsValue(
+                collect(array_merge($beam->flags ?? [], ['PAUSED']))
+                    ->unique()
+                    ->map(fn ($flag) => ['flag' => $flag, 'enabled' => true])->all()
+            )]);
+
+            Log::info("Pausing beam {$beam->code} cause the collection {$beam->collection_chain_id} was paused.");
+        }
     }
 }
