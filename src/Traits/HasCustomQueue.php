992d6b626b7ccca5bdd8c4d994595f35eed49b29
Abner Tudtud <abner@enjin.io>
2024-11-06 09:00:18 +0800

    refactor

--- src/Traits/HasCustomQueue.php
+++ src/Traits/HasCustomQueue.php
@@ -4,13 +4,13 @@ namespace Enjin\Platform\Beam\Traits;
 
 trait HasCustomQueue
 {
-    protected function setQueue(): void
-    {
-        $this->onQueue(config('enjin-platform-beam.queue'));
-    }
-
     public function viaQueue(): string
     {
         return config('enjin-platform-beam.queue');
+    }
+
+    protected function setQueue(): void
+    {
+        $this->onQueue(config('enjin-platform-beam.queue'));
     }
 }
